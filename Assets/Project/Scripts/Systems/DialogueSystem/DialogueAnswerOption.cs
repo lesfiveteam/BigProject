6c0a8aa76b9afe442efb63dc8947a7f589457dfc
Семён <cemenbond@mail.ru>
2026-04-12 21:18:00 +0300

    функционал диалоговой системы

--- Assets/Project/Scripts/Systems/DialogueSystem/DialogueAnswerOption.cs
+++ Assets/Project/Scripts/Systems/DialogueSystem/DialogueAnswerOption.cs
@@ -5,8 +5,19 @@ namespace BigProject.Systems.DialogueSystem
     [CreateAssetMenu(fileName = "DialogueAnswerOption", menuName = "Scriptable Objects/DialogueAnswerOption")]
     public class DialogueAnswerOption : ScriptableObject
     {
+        // Ñonsider the hash string and the name Dialogue Line to be unique values
+        public string HashId { 
+            get 
+            { 
+                return this.Text.GetHashCode() 
+                        + ":" 
+                        + (this.DialogueLine ? this.DialogueLine.name.GetHashCode() : "-")
+                        + ":"
+                        + this.name; 
+            } 
+        }
         public string Text;
         public DialogueLine DialogueLine;
-        public bool IsStoryOption = false;
+        public bool IsChosenByDefault = false;
     }
 }
