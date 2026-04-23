ffa6cfefde6d203459ae783811229e858600972b
Insearchman <insearchman@gmail.com>
2026-04-23 17:45:34 +0300

    [Village] Add workers

---
+++ Assets/Project/Scripts/NPC/NPCWorker.cs
@@ -0,0 +1,36 @@
+﻿using BigProject.Utilities;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Project.Scripts.NPC
+{
+    public class NPCWorker : MonoBehaviour
+    {
+        private readonly int StartTrigger = Animator.StringToHash("Start");
+
+        private float _timeToAlive = 2f;
+
+        [SerializeField] private Animator _animator;
+
+        private Coroutine _animationCoroutine;
+
+        private void Start()
+        {
+            ExceptionUtilities.ThrowIfNullFormat(_animator);
+
+            _animationCoroutine = StartCoroutine(StartAnimations());
+        }
+
+        private IEnumerator StartAnimations()
+        {
+            yield return new WaitForSeconds(Random.Range(0, _timeToAlive));
+
+            _animator.SetTrigger(StartTrigger);
+        }
+
+        private void OnDestroy()
+        {
+            StopCoroutine(_animationCoroutine);
+        }
+    }
+}
