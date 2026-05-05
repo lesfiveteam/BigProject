a10a9aeecf153ff53b29089eede74972617bfee9
Malorepa <vladimirfrol@mail.ru>
2026-05-05 14:25:56 +0300

    Новый переход между 1 и 2 квестом

--- Assets/Project/Scripts/Gameplay/VillageBetweenFirstAndSecond/QuestBoundariesController.cs
+++ Assets/Project/Scripts/Gameplay/VillageBetweenFirstAndSecond/QuestBoundariesController.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 using BigProject.Systems.QuestSystem;
 using UnityEngine.Assertions;
 using BigProject.Systems;
+using BigProject.Systems.Inventory;
+using BigProject.Managers;
+using BigProject.Managers.CutsceneManager;
 
 namespace BigProject.Gameplay.VillageBetweenFirstAndSecond
 {
     public class QuestBoundariesController : MonoBehaviour, IQuestBoundariesController
     {
+        [SerializeField]
+        private QuestActions _questActions;
         [SerializeField]
         private GameObject _questObjects;
         [SerializeField]
@@ -17,12 +22,14 @@ namespace BigProject.Gameplay.VillageBetweenFirstAndSecond
 
         private void Awake()
         {
+            Assert.IsNotNull(_questActions, string.Format(LogStr.CRITICAL_NOT_SERIALIZED_FIELD, gameObject.name, "QuestActions"));
             Assert.IsNotNull(_questObjects, string.Format(LogStr.CRITICAL_NOT_SERIALIZED_FIELD, gameObject.name, "Quest Objects"));
-            Assert.IsNotNull(_questObjects, string.Format(LogStr.CRITICAL_NOT_SERIALIZED_FIELD, gameObject.name, "Miller"));
+            Assert.IsNotNull(_miller, string.Format(LogStr.CRITICAL_NOT_SERIALIZED_FIELD, gameObject.name, "Miller"));
         }
 
         public void InitOnSceneEntry()
         {
+            _questActions.Init(ServiceLocator.GetService<InventorySystem>(), ServiceLocator.GetService<CutsceneManager>());
             _questObjects.SetActive(true);
             _miller.SetActive(true);
         }
