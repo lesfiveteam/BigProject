1466d9e3f4aa7d76d64bbfebb3289dd6c40b5076
Insearchman <insearchman@gmail.com>
2026-05-07 02:11:54 +0300

    [NPC] Add sounds to animals

---
+++ Assets/Project/Scripts/NPC/Animals/Deer/NPCDeerPlaySound.cs
@@ -0,0 +1,20 @@
+﻿using BigProject.Utilities;
+using UnityEngine;
+
+namespace Assets.Project.Scripts.NPC.Animals.Deer
+{
+    public class NPCDeerPlaySound : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _audioSource;
+
+        private void Start()
+        {
+            ExceptionUtilities.ThrowIfNullFormat(_audioSource);
+        }
+
+        private void PlaySound()
+        {
+            _audioSource.Play();
+        }
+    }
+}
