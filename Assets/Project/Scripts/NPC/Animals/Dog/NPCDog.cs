e50baa0f94f8d6d28f4097de224baf0fdbdebd09
Insearchman <insearchman@gmail.com>
2026-05-10 06:05:08 +0300

    [Ref] Dog

--- Assets/Project/Scripts/NPC/Animals/Dog/NPCDog.cs
+++ Assets/Project/Scripts/NPC/Animals/Dog/NPCDog.cs
@@ -1,15 +1,21 @@
-﻿using UnityEngine;
+﻿using BigProject.Utilities;
+using UnityEngine;
 
 namespace Assets.Project.Scripts.NPC.Animals.Dog
 {
     public class NPCDog : MonoBehaviour, IScared, IUnscared
     {
-        private readonly int ActionBool = Animator.StringToHash("isActive");
+        private readonly int _actionBool = Animator.StringToHash("isActive");
 
         [SerializeField] private Animator _animator;
 
-        public void Scare(Transform danger) => _animator.SetBool(ActionBool, true);
+        private void Start()
+        {
+            ExceptionUtilities.ThrowIfNullFormat(_animator);
+        }
 
-        public void Unscare() => _animator.SetBool(ActionBool, false);
+        public void Scare(Transform danger) => _animator.SetBool(_actionBool, true);
+
+        public void Unscare() => _animator.SetBool(_actionBool, false);
     }
 }
